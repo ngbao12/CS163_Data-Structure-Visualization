634dfbbedb9cec68e476a2ec35db02413b8b4436
ngbao12 <ngocbao6422@gmail.com>
2024-08-15 09:12:47 +0700

    implement button, button image, change name of variable

--- project/project/GUI.cpp
+++ project/project/GUI.cpp
@@ -1,1 +1,44 @@
 #include "GUI.hpp"
+
+Button::Button(Rectangle rect, const std::string text, float yText, Color textColor, float fontSize, Font font) {
+    this->outerRect = rect;
+    this->content = text;
+    this->font = font;
+    this->fontSize = fontSize;
+    this->contentColor = textColor;
+    Vector2 textSize = MeasureTextEx(this->font, text.c_str(), this->fontSize,2);
+    this->contentPos = {this->outerRect.x + (this->outerRect.width-textSize.x)/2, (yText == -1) ? (rect.y + (rect.height - textSize.y)/2) : yText};
+    this->isHovered = false;
+}
+
+void Button::draw(float radius) {
+    this->isHovered = CheckCollisionPointRec(GetMousePosition(), this->outerRect);
+    DrawRectangleRounded(this->outerRect, radius/100, 32, this->isHovered ? HOVERED_BUTTON_LIGHT_THEME : NONHOVERED_BUTTON_LIGHT_THEME);
+    DrawTextEx(FONT, this->content.c_str(), this->contentPos, this->fontSize, 2, this->contentColor);
+}
+
+int Button::handle() {
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && this->isHovered) {
+        return 1;
+    }
+    return 0;
+}
+
+ButtonImage::ButtonImage(Rectangle outerRect, Rectangle innerRect, const char* path, const std::string text, float yText, Color textColor, float fontSize, Font font) {
+    this->outerRect = outerRect;
+    this->innerRect = innerRect;
+    this->texture = LoadTexture(path);
+    this->content = text;
+    this->contentPos = {this->outerRect.x + (this->outerRect.width - MeasureTextEx(font, text.c_str(), fontSize, 5).x)/2, yText};
+    this->contentColor = textColor;
+    this->fontSize = fontSize;
+    this->font = font;
+    
+}
+
+void ButtonImage::draw(float radius) {
+    DrawRectangleRounded(outerRect, radius/100, 32,this->isHovered ? HOVERED_BUTTON_LIGHT_THEME : NONHOVERED_BUTTON_LIGHT_THEME);
+    drawPicture(this->texture, this->innerRect);
+    DrawTextPro(this->font, this->content, this->contentPos, ORIGIN, 0, this->fontSize, 2, this->contentColor);
+}
+
--- project/project/GUI.hpp
+++ project/project/GUI.hpp
@@ -1,6 +1,34 @@
 #ifndef GUI_hpp
 #define GUI_hpp
 
+#include "General.hpp"
 
+class Button {
+protected:
+    Rectangle outerRect;
+    std::string content;
+    Vector2 contentPos;
+    Color contentColor;
+    Font font;
+    float fontSize;
+    bool isHovered;
+public:
+    Button(Rectangle rect, const std::string text, float yText, Color textColor, float fontSize, Font font = FONT);
+    Button() : Button({0,0,0,0}, "", 0.0f, HOVERED_BUTTON_LIGHT_THEME, 0) {}
+    void draw(float radius = 15);
+    int handle();
+    bool getIsHovered() {return this->isHovered;}
+    
+    virtual ~Button() = default;
+};
 
+class ButtonImage : public Button {
+protected:
+    Rectangle innerRect;
+    Texture2D texture;
+public:
+    ButtonImage(Rectangle outerRect, Rectangle innerRect, const char* path, const std::string text, float yText, Color textColor, float fontSize, Font font=FONT);
+    ButtonImage() : Button() {}
+    void draw(float radius = 15);
+};
 #endif /* GUI_hpp */
