c49f98b081b2da1b83e9174fc6cdbf7d51e75484
ngbao12 <ngocbao6422@gmail.com>
2024-08-25 00:02:02 +0700

    variable for AVL visualize

--- project/project/AVL.hpp
+++ project/project/AVL.hpp
@@ -55,45 +55,68 @@ struct AVLStep {
 };
 
 class AVLTree {
-private:
-    AVLNode* root;
-    
-    void freeTree(AVLNode* &root);
-    void freeStep(AVLStep &step);
-    void clearProcess();
-    
-    std::vector<AVLStep> process;
-    AVLNode* cloneTree(AVLNode* node);
-    
-    void saveStep(int speacialValue, int type, std::vector<int> line, const std::string infor, const std::string code, bool forCreate = false);
-    void updateStandardPos(AVLNode* root, Vector2 parentPos = {1013, 100}, Vector2 delta = {0.0f, 0.0f});
-    void updateTargetPos(AVLNode* root, Vector2 parentPos = {1013,100}, Vector2 delta = {0.0f, 0.0f}, bool forCreate = false);
-    
-    int height (AVLNode* node) {
-        return node ? node->height : 0;
-    }
-    
-    int getBalance(AVLNode* node) {
-        return node ? height(node->left) - height(node->right) : 0;
-    }
-    
-    AVLNode* rightRotate(AVLNode* y);
-    AVLNode* leftRotate(AVLNode* x);
-    
-    void updateHeight(AVLNode* node) {
-        node->height = 1 + std::max(height(node->left), height(node->right));
-    }
-    
-    int updateAllHeight(AVLNode* node);
-    std::pair<AVLNode*, int> balance(AVLNode* node, const std::string code);
-public:
-    AVLTree() : root(nullptr) {}
-    std::vector<AVLStep> getProcess() {return this->process;}
-    void createFromFile (const char* filename);
-    void createWithRandomizedData(int n, int range);
-    void insert(int key, Vector2 start = {1013,100}, Vector2 end = {1013,100});
-    void deleteNode(int key);
-    bool search(int key);
-    void printInOrder();
+    private:
+        AVLNode* root;
+        
+        void freeTree(AVLNode* &root);
+        void freeStep(AVLStep &step);
+        void clearProcess();
+        
+        std::vector<AVLStep> process;
+        AVLNode* cloneTree(AVLNode* node);
+        
+        void saveStep(int speacialValue, int type, std::vector<int> line, const std::string infor, const std::string code, bool forCreate = false);
+        void updateStandardPos(AVLNode* root, Vector2 parentPos = {1013, 100}, Vector2 delta = {0.0f, 0.0f});
+        void updateTargetPos(AVLNode* root, Vector2 parentPos = {1013,100}, Vector2 delta = {0.0f, 0.0f}, bool forCreate = false);
+        
+        int height (AVLNode* node) {
+            return node ? node->height : 0;
+        }
+        
+        int getBalance(AVLNode* node) {
+            return node ? height(node->left) - height(node->right) : 0;
+        }
+        
+        AVLNode* rightRotate(AVLNode* y);
+        AVLNode* leftRotate(AVLNode* x);
+        
+        void updateHeight(AVLNode* node) {
+            node->height = 1 + std::max(height(node->left), height(node->right));
+        }
+        
+        int updateAllHeight(AVLNode* node);
+        std::pair<AVLNode*, int> balance(AVLNode* node, const std::string code);
+    public:
+        AVLTree() : root(nullptr) {}
+        std::vector<AVLStep> getProcess() {return this->process;}
+        void createFromFile (const char* filename);
+        void createWithRandomizedData(int n, int range);
+        void insert(int key, Vector2 start = {1013,100}, Vector2 end = {1013,100});
+        void deleteNode(int key);
+        bool search(int key);
+        void printInOrder();
 };
+
+class AVLTreeVisualize {
+private:
+    AVLTree tree;
+    AVLStep step;
+    int ype = 0;
+    ProgressBar progressBar;
+    int stepIndex;
+    int frame;
+    int numFrameOfAnimation;
+    
+    Button createButton;
+    bool isCreateChosen;
+    Button deleteButton;
+    bool isDeteleChosen;
+    Button searchButton;
+    bool isSearchChosen;
+    InputStr inputNumber;
+    Button playButton;
+    
+    Font font;
+};
+
 #endif /* AVL_hpp */
